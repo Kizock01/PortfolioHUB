320458268f3ce6b121391262f3e1d2cbbd0ab16b
gbonatti <gbonatti@gmail.com>
2026-06-09 18:56:32 -0300

    Implementa logica de senha secreta

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+SENHA_SECRETA = 42
+
+
 def mostrar_intro():
     print("=== Adivinha Senha ===")
     print("Voce acordou dentro de uma sala digital trancada.")
@@ -7,6 +10,14 @@ def mostrar_intro():
 
 def main():
     mostrar_intro()
+    tentativa = int(input("Digite sua tentativa: "))
+
+    if tentativa == SENHA_SECRETA:
+        print("Acertou! A porta foi desbloqueada.")
+    elif tentativa > SENHA_SECRETA:
+        print("Muito alto.")
+    else:
+        print("Muito baixo.")
 
 
 if __name__ == "__main__":
